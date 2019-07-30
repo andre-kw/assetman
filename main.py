107c68a2e65390b9125bb3e74c653e3f0caa9f36
Andre K. W <andre.k.willie@gmail.com>
2019-07-30 17:16:26 -0400

    fixed parser; created /xml/save route

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import json
-import xml.etree.ElementTree as ElementTree
-from flask import Flask, escape, request
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, ElementTree
+from flask import Flask, escape, request, jsonify
 
 dcr_url = 'http://habboo-a.akamaihd.net/dcr/hof_furni/{}'
 res_url = 'https://habbo.com/{}'
@@ -14,7 +15,7 @@ def get_furnidata():
         f.write(res.content)
 
 def parse_furnidata(xmlfile):
-    tree = ElementTree.parse(xmlfile)
+    tree = ET.parse(xmlfile)
     root = tree.getroot()
 
     fd = []
@@ -25,6 +26,15 @@ def parse_furnidata(xmlfile):
         for child in item:
             if(child.text):
                 item_dict[child.tag] = child.text
+            elif(child):
+                subitems = []
+
+                for subchild in child:
+                    subitem_dict = {}
+                    subitem_dict[subchild.tag] = subchild.text
+                    subitems.append(subitem_dict)
+
+                item_dict[child.tag] = subitems
 
         fd.append(item_dict)
 
@@ -41,17 +51,53 @@ app = Flask(__name__)
 def hello():
     return 'hmmm?'
 
-@app.route('/furnitype/<int:id>')
-def get_furnitype(id):
-    #node = list(filter(lambda furnitype: furnitype['id'] == id, fd))
+@app.route('/furnitype/<int:id>', methods=['GET'])
+def route_furnitype(id):
+    res = {}
     node = next((item for item in fd if int(item['id']) == id), False)
-    print(node)
 
     if node:
-        res = json.dumps(node)
-        return res
+        res = node
 
-    return 'didnt find it'
+    return jsonify(res)
 
-# TODO: figure out GET and POSTs
-#@app.route('/furnitype/<int:id>/<str:attribute>')
+
+#@app.route('/furnitype/<int:id>/<string:attribute>')
+
+
+# TODO: missing 'adurl' and 'customparams' (which are normally empty)
+@app.route('/xml/save')
+def route_xml_save():
+    root = Element('furnidata')
+    rooms = Element('roomitemtypes')
+    walls = Element('wallitemtypes')
+
+    for ft_dict in fd:
+        ft = Element('furnitype')
+
+        for key, val in ft_dict.items():
+            if(key != 'id' and key != 'classname'):
+                child = Element(key)
+
+                if(isinstance(val, list)):
+                    for d in val:
+                        for k, v in d.items():
+                            subchild = Element(k)
+                            subchild.text = v
+                            child.append(subchild)
+                else:
+                    child.text = val
+
+                ft.append(child)
+            else:
+                ft.set(key, val)
+
+        rooms.append(ft)
+        #print(ft)
+
+    root.append(rooms)
+    root.append(walls)
+    tree = ElementTree(root)
+    tree.write(open('xml/furnidata-sea.xml', 'wb'), encoding='utf-8', xml_declaration=True)
+
+    return 'hey'
