def1c86ef58b50ff4a8ee9c6b409e120aee9ba3a
Andre K. W <andre.k.willie@gmail.com>
2019-07-31 20:25:26 -0400

    switched to app.make_response()

--- assetextract/views.py
+++ assetextract/views.py
@@ -16,7 +16,7 @@ def get_furnitype(type, id):
     else:
         res = {}
 
-    return res
+    return jsonify(res)
 
 def patch_furnitype(type, id, form):
     dic = fd.wallitemtypes if type == 'wall' else fd.roomitemtypes
@@ -30,27 +30,43 @@ def patch_furnitype(type, id, form):
 
         dic[id] = new_item
 
-    res = Response({}, status=204)
-    res.headers['Location'] = request.base_url
-    return res
+    return app.make_response(({}, 204, {'Location': request.base_url}))
+
+def post_furnitype(type, id, form):
+    dic = fd.wallitemtypes if type == 'wall' else fd.roomitemtypes
+
+    if id in dic:
+        #return Response({'error': 'ID already exists.'}, status=409)
+        return app.make_response(({'error': 'ID already exists'}, 409))
+
+    item = {
+        'bc': request.form.get('bc', '0'),
+        'buyout': request.form.get('buyout', '1')
+    }
+    
+    return app.make_response(({}, 201, {'Location': request.base_url}))
 
 
 # routes
-@app.route('/furnidata/room/furnitype/<int:id>', methods=['GET', 'PATCH'])
+@app.route('/furnidata/room/furnitype/<int:id>', methods=['GET', 'PATCH', 'POST'])
 def route_room_furnitype(id):
     if request.method == 'GET':
-        res = jsonify(get_furnitype('room', id))
+        res = get_furnitype('room', id)
     elif request.method == 'PATCH':
         res = patch_furnitype('room', id, request.form)
+    elif request.method == 'POST':
+        res = post_furnitype('room', id, request.form)
 
     return res
 
-@app.route('/furnidata/wall/furnitype/<int:id>', methods=['GET', 'PATCH'])
+@app.route('/furnidata/wall/furnitype/<int:id>', methods=['GET', 'PATCH', 'POST'])
 def route_wall_furnitype(id):
     if request.method == 'GET':
-        res = jsonify(get_furnitype('wall', id))
+        res = get_furnitype('wall', id)
     elif request.method == 'PATCH':
         res = patch_furnitype('wall', id, request.form)
+    elif request.method == 'POST':
+        res = post_furnitype('wall', id, request.form)
 
     return res
 
@@ -65,8 +81,6 @@ def route_furnidata_actions():
     if actions[action]:
         actions[action]()
 
-    res = Response({}, status=201)
-
-    return res
+    return app.make_response(({}, 201))
 
 #@app.route('/furnidata/xml/<string:filename>')
